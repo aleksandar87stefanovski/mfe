87da8b5dbd5e424d6d231c0fce91e63913eb6b0b
Aleksandar Stefanovski <aleksandar.stefanovski@wsa.com>
2022-10-12 16:40:35 +0200

    test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return <div>
-     <h1>Hi there from container!!!</h1>  
+     <h1>Hi Ekce from container!!! Macu ti programeriste morej!</h1>  
      <hr />
      <MarketingApp />
   </div>
